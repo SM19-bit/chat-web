548225b69720f54eccdbe2fcab44646864a35363
SM19-bit <surojeetmanna8@gmail.com>
2025-07-12 00:30:33 +0530

    Add a short message describing your update

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -28,7 +28,7 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
       <div className='flex flex-col'>
         {userDummyData.map((user, index)=>(
           <div onClick={() => {setSelectedUser(user)}} // set selected user
-          key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm $(selectedUser?._id === user._id && 'bg-[#282142]/50')`}> 
+          key={index}className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id ? 'bg-[#282142]/50' : ''}`}> {/* i changed manually*/}
             <img src={user?.profilePic || assets.avatar_icon} alt=""
             className='w-[35px] aspect-[1/1] rounded-full' />
             <div className='flex flex-col leading-5'>
